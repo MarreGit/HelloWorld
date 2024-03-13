6dd7ab56d5f7879376cc1dec2fd523cbe6bf39b9
MarreGit <148989756+MarreGit@users.noreply.github.com>
2024-03-13 14:44:01 +0100

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello cat!");
-        System.out.println("Hello Dog!");
+        System.out.println("Hello cats!");
+        System.out.println("Hello dogs!");
 
         System.out.println("Hello world!");
     }
