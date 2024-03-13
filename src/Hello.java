60af26cb88a405232b398f92724f9b074157b835
Mats <mats.lonnstrom@gmail.com>
2024-03-13 14:47:47 +0100

    Even more code

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello cats!");
         System.out.println("Hello dogs!");
-
+        System.out.println("Even more code");
         System.out.println("Hello world. How are you!");
     }
 }
