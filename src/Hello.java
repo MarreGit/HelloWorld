dd765d12e199de308170dd178f98b39c56842b32
Mats <mats.lonnstrom@gmail.com>
2024-03-13 14:43:04 +0100

    Hello world

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello cat!");
         System.out.println("Hello Dog!");
+
+        System.out.println("Hello world!");
     }
 }
